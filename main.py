1857aba3332035cde1be0bf6b4bc6a08d751bec0
L <240835891+yanetester@users.noreply.github.com>
2026-05-14 03:38:26 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Мой ToDo-лист")
+
+entry = tk.Entry(window, width=40)
+entry.pack(pady=10)
+
+
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+
+
+btn = tk.Button(window, text="Добавить задачу", command=add_task)
+btn.pack()
+
+listbox = tk.Listbox(window, width=50)
+listbox.pack(pady=10)
+
+window.mainloop()
+""""""
